3f0426eb14d0a436a428c608d206670fd3c39893
Evgeny Gurin <rypylook@outlook.com>
2018-11-20 03:16:17 +0000

    fixed log monitor

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -3,14 +3,8 @@
 nginx
 
 letsencrypt certonly --webroot -w /var/www/rypy -d rypy.ru -d www.rypy.ru -m rypylook@outlook.com --agree-tos
-
-mv /nginx2.conf /etc/nginx/nginx.conf
-
-nginx -s reload
-
-echo "30 8 * * 1 /usr/bin/letsencrypt renew >> /var/log/letsencrypt-renew.log" >> /etc/cron.d/sample
-
 service rsyslog start && service cron start && tail -f /var/log/syslog
 
-nginx -g 'daemon off;' 
+#mv /nginx2.conf /etc/nginx/nginx.conf
 
+nginx -s reload
